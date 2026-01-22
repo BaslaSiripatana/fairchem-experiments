ee54faa7002f2ca78cebcbf8e4d82480ddb81c8e
Basla  Siripatana <siripab@gpu609-03.ibex.kaust.edu.sa>
2026-01-22 10:33:30 +0300

    qm9_training_work_finetune_error

---
+++ my_scripts/run_uma_qm9_finetune.sh
@@ -0,0 +1,30 @@
+#!/bin/bash --login
+#SBATCH -N 1
+#SBATCH --job-name=uma_qm9_finetune
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --time=2:00:00
+#SBATCH -o logs/%x.%j.out
+#SBATCH -e logs/%x.%j.err
+#SBATCH --mail-type=FAIL
+
+hostname
+nvidia-smi
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate fairchem
+
+export OMP_NUM_THREADS=8
+
+export WANDB_MODE=offline
+
+# export WANDB_ENTITY=theme4
+# export WANDB_PROJECT=Fairchem
+# export WANDB_RUN_GROUP=uma_qm9_finetune
+
+cd ~/fairchem || exit 1
+
+python main.py \
+  -c configs/uma/finetune/uma_sm_finetune_qm9.yaml
--- my_scripts/simple_batch_sampler.py
+++ my_scripts/simple_batch_sampler.py
@@ -1,43 +1,4 @@
-# from torch.utils.data.distributed import DistributedSampler
-# from torch.utils.data import RandomSampler, BatchSampler
-# import torch
-
-# class SimpleStatefulBatchSampler:
-#     def __init__(self, sampler):
-#         self.sampler = sampler
-#         self.epoch = 0
-#         self.start_iter = 0
-
-#     def __iter__(self):
-#         it = iter(self.sampler)
-#         for _ in range(self.start_iter):
-#             next(it, None)
-#         return it
-
-#     def __len__(self):
-#         return len(self.sampler)
-
-#     def set_epoch_and_start_iteration(self, epoch: int, step_start: int):
-#         self.epoch = epoch
-#         self.start_iter = step_start
-
-
-# def simple_batch_sampler_fn(
-#     dataset,
-#     batch_size: int,
-#     num_replicas: int = 1,
-#     rank: int = 0,
-# ):
-#     # minimal: ignore num_replicas/rank but accept them
-#     base_sampler = torch.utils.data.BatchSampler(
-#         torch.utils.data.RandomSampler(dataset),
-#         batch_size=batch_size,
-#         drop_last=False,
-#     )
-#     return SimpleStatefulBatchSampler(base_sampler)
-
-
-
+################### for training ######################
 
 from torch.utils.data import RandomSampler, BatchSampler
 
@@ -85,3 +46,52 @@ def simple_batch_sampler_fn(
     )
 
     return SimpleStatefulBatchSampler(batch_sampler)
+
+##################### for finetune ########################
+
+import torch
+from torch.utils.data import RandomSampler, SequentialSampler, BatchSampler
+
+class SimpleStatefulBatchSamplerFinetune:
+    """
+    Yields List[int] batches (indices).
+    Must expose set_epoch_and_start_iteration for FairChem MLIPTrainEvalUnit.
+    """
+    def __init__(self, batch_sampler):
+        self.batch_sampler = batch_sampler
+        self.epoch = 0
+        self.start_iter = 0
+
+    def __iter__(self):
+        it = iter(self.batch_sampler)
+        for _ in range(self.start_iter):
+            next(it, None)
+        return it
+
+    def __len__(self):
+        return len(self.batch_sampler)
+
+    def set_epoch_and_start_iteration(self, epoch: int, step_start: int):
+        self.epoch = epoch
+        self.start_iter = step_start
+
+def make_stateful_batch_sampler(
+    dataset,
+    batch_size: int,
+    shuffle: bool = True,
+    seed: int = 0,
+    drop_last: bool = False,
+):
+    if shuffle:
+        g = torch.Generator()
+        g.manual_seed(seed)
+        sampler = RandomSampler(dataset, generator=g)
+    else:
+        sampler = SequentialSampler(dataset)
+
+    batch_sampler = BatchSampler(
+        sampler,
+        batch_size=batch_size,
+        drop_last=drop_last,
+    )
+    return SimpleStatefulBatchSamplerFinetune(batch_sampler)
